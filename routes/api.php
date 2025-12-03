1ad4a38ef36e251fa99f65368453ac4eb6ec1c63
Dbeltcol <dbeltrandev@gmail.com>
2025-12-02 23:57:05 -0500

    feat(user) Create api rest for users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('login', [AuthController::class, 'login']);
+Route::resource('users', UserController::class);
